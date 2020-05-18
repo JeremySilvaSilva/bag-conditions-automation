083a2f2809bb89cfe736dadd35d168f93d61658d
JeremyAndress <jeremy.a.silva.silva@gmail.com>
2020-05-18 16:22:34 -0400

    ADD : boostrap paginator

---
+++ src/app/home/values.py
@@ -0,0 +1,11 @@
+
+
+class Factor_values:
+    factor_all = [
+        'id','name_tag',
+        'desc','customerTypeTitle',
+        'customerTypeCode','customerSubTypeCode',
+        'alevel','atype', 'cid',
+        'cstatus', 'pind', 'plantype',
+        'mname', 'stype','oname' 
+    ]
--- src/app/home/views.py
+++ src/app/home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from app.builder.models import (
+    Factor
+)
+from .values import Factor_values
+from django.core.paginator import Paginator
 
 def index(request):
-    return render(request, 'home/index.html')
+    values = Factor_values.factor_all
+    factor = Factor.objects.values(*values).order_by('id')
+    paginator = Paginator(factor,4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
+    return render(request, 'home/index.html',context)
 
